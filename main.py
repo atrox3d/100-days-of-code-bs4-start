75ecac773e8f6a60a67c122ca6ea519abfa36cd1
atrox3d <atrox3d@gmail.com>
2021-03-06 11:36:30 +0100

    find tags, get attributes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import bs4
 from bs4 import BeautifulSoup
 # import lxml
 
@@ -19,13 +20,39 @@ else:
 
 # soup = BeautifulSoup(contents, "lxml")
 soup = BeautifulSoup(contents, "html.parser")
-print(soup.title)
-print(soup.title.name)
-
+#
+#   print all html
+#
 # print(soup)
 print(soup.prettify())
-
-print(soup.a)   # get the first a tag
-print(soup.li)  # get the first li tag
-
-
+#
+#   print some tags
+#
+print(soup.title)
+print(soup.title.name)
+#
+print(soup.a)                           # get the first a tag
+print(soup.li)                          # get the first li tag
+#
+#   get collection of tags
+#
+a_tags = soup.find_all(name="a")        # get all of the anchor tags
+print(a_tags)
+#
+p_tags = soup.find_all(name="p")        # get all of the paragraph tags
+print(p_tags)
+#
+#   print tag attributes
+#
+tag: bs4.element.Tag = None
+for tag in a_tags:
+    print(tag.getText())                # get the text inside the tag
+    print(tag.get("href"))              # get the content of the href attribute
+#
+#   find a specific tag
+#
+h1_name: bs4.element.Tag = soup.find(name="h1", id="name")
+print(h1_name)
+#
+section_heading: bs4.element.Tag = soup.find(name="h3", class_="heading")
+print(section_heading.get("class"))
